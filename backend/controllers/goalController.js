451cd8fb67943165e828fa259445224dbe55614a
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2023-04-19 15:40:42 +0600

    add crud operation with node express mongodb

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,73 @@
+const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel');
+
+//@desc  GET goals
+//@route  GET /api/goals
+//@access  Private
+const getGoals = asyncHandler(async (req,res) =>{
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
+})
+
+
+//@desc  SET goals
+//@route  POST  /api/goals
+//@access  Private
+const setGoal = asyncHandler(async(req,res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal);
+})
+
+
+
+//@desc  Update goal
+//@route  PUT /api/goals/:id
+//@access  Private
+const updateGoal = asyncHandler(async(req,res)=>{
+    const goal = await Goal.findById(req.params.id);
+    
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(
+        req.params.id,
+         req.body,
+        {new:true} //create new if there is no exist
+    )
+
+    res.status(200).json(updatedGoal);
+})
+
+//@desc  Delete goals
+//@route  DELETE /api/goals/:id
+//@access  Private
+const deleteGoal = asyncHandler(async(req,res)=>{
+    const goal = await Goal.findById(req.params.id);
+    
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await Goal.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({message:`delete goal ${req.params.id}`});
+})
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express =require('express');
+const { getGoals } = require('./controllers/goalController');
+const dotenv = require('dotenv').config();
+const goalRoutes = require('./routes/goalRoutes');
+const errorHandler = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const port = process.env.PORT ||  5000;
+
+
+connectDB();
+
+const app = express();
+
+//middleware for Row json
+app.use(express.json());
+//middleware for extended url data
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/goals',goalRoutes);
+
+app.use(errorHandler)
+
+
+
+app.listen(port,()=>console.log(`listening on port ${port}`))
