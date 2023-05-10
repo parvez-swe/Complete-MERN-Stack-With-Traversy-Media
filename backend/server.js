9b879a76c362b142bff3b8b2a0b391683f2aa259
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2023-05-10 11:56:11 +0600

    add all

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    methods: ["Get", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     origin: [process.env.FRONTEND_URL_1],
   })
 );
